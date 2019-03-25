0aa84908f43630211872b6fdd8894a955f62b142
Ederson <funescastilloederson@gmail.com>
2019-03-26 00:53:26 +0100

    Uploaded different Expectation results
    * Each line shows ELO1, ELO2, and Exp(ELO1,ELO2) obtained when changing the X in [1.0 / (1 + math.pow(10, -((Ra - Rb) / X))) ]

--- testing.py
+++ testing.py
@@ -24,6 +24,7 @@ problem_gain = []
 for x in u:
 	for y in p:
 
+		"""	
 		n_x, n_y = ELO.simulate_no_tries(x, y,'AC')	
 
 		if n_x-x != 0 and n_y-y != 0:
@@ -31,7 +32,6 @@ for x in u:
 			user_gain.append(n_x-x)
 			problem_gain.append(n_y-y)
 
-		"""	
 		n_x, n_y = ELO.simulate_no_tries(x, y,'WA')	
 
 		if n_x-x != 0 and n_y-y != 0:
@@ -39,9 +39,9 @@ for x in u:
 			user_gain.append(n_x-x)
 			problem_gain.append(n_y-y)
 
-		#print(x,y,ELO.Expectation(x,y))
 		"""
+		print(x,y,ELO.Expectation(x,y))
 
-ACR_Stats.show_ELO_gain(elo_differences, user_gain, problem_gain,x_label = "USER ELO - PROBLEM ELO", y_label="OLD ELO - NEW ELO", title=f"ELO Gain For Different ELO Differences", filename=f"ELO Gain (New K-Factor Formula x 0.25) (New Expectation) [from testing.py] - No Zeros .png")
+#ACR_Stats.show_ELO_gain(elo_differences, user_gain, problem_gain,x_label = "USER ELO - PROBLEM ELO", y_label="OLD ELO - NEW ELO", title=f"ELO Gain For Different ELO Differences", filename=f"ELO Gain (New K-Factor Formula x 0.25) (New Expectation) [from testing.py] - No Zeros .png")
 #ACR_Stats.show_scatter(elo_differences, user_gain, "User Gain",x_label = "USER ELO - PROBLEM ELO", y_label="OLD ELO - NEW ELO", title="ELO Gain For Different ELO Differences")
 #ACR_Stats.show_scatter(elo_differences, problem_gain, "Problem Gain",x_label = "USER ELO - PROBLEM ELO", y_label="OLD ELO - NEW ELO", title="ELO Gain For Different ELO Differences")
